17f01e0c7e670e3fc37dc1373596d6546a2ec5ff
Wong Kai Yin <43575835+kaiyin78@users.noreply.github.com>
2022-03-30 19:57:28 +0800

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -2,13 +2,13 @@ import pyrebase
 
 
 firebaseConfig = {
-  'apiKey': "AIzaSyAbBYgZUyVfdw-9Jyx7IREo1Wv82em_Hu4",
-  'authDomain': "fypars-660c5.firebaseapp.com",
-  'projectId': "fypars-660c5",
-  'storageBucket': "fypars-660c5.appspot.com",
-  'messagingSenderId': "810959590520",
-  'appId': "1:810959590520:web:0dbad49b86bf784e3d898d",
-  'databaseURL': "https://fypars-660c5-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  'apiKey': "*",
+  'authDomain': "*",
+  'projectId': "*",
+  'storageBucket': "*",
+  'messagingSenderId': "*",
+  'appId': "*",
+  'databaseURL': "*"
 }
 
 firebase = pyrebase.initialize_app(firebaseConfig)
